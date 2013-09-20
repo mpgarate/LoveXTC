35875a5ae3b4a4d39ef885acc6b5af9ffeb4f76d
vagrant <mpgarate@gmail.com>
2013-09-20 23:55:55 +0000

    added makefile and Translator class from in-class example

---
+++ Translator.java
@@ -0,0 +1,114 @@
+package xtc.oop;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+import xtc.lang.JavaFiveParser;
+import xtc.lang.JavaPrinter;
+
+import xtc.parser.ParseException;
+import xtc.parser.Result;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+import xtc.util.Tool;
+
+/**
+ * A translator from (a subset of) Java to (a subset of) C++.
+ */
+public class Translator extends Tool {
+
+  /** Create a new translator. */
+  public Translator() {
+    // Nothing to do.
+  }
+
+  public String getName() {
+    return "Java to C++ Translator";
+  }
+
+  public String getCopy() {
+    return "(C) 2013 <Group Name>";
+  }
+
+  public void init() {
+    super.init();
+
+    // Declare command line arguments.
+    runtime.
+      bool("printJavaAST", "printJavaAST", false, "Print Java AST.").
+      bool("printJavaCode", "printJavaCode", false, "Print Java code.").
+      bool("countMethods", "countMethods", false, "Count all Java methods.");
+  }
+
+  public void prepare() {
+    super.prepare();
+
+    // Perform consistency checks on command line arguments.
+  }
+
+  public File locate(String name) throws IOException {
+    File file = super.locate(name);
+    if (Integer.MAX_VALUE < file.length()) {
+      throw new IllegalArgumentException(file + ": file too large");
+    }
+    return file;
+  }
+
+  public Node parse(Reader in, File file) throws IOException, ParseException {
+    JavaFiveParser parser =
+      new JavaFiveParser(in, file.toString(), (int)file.length());
+    Result result = parser.pCompilationUnit(0);
+    return (Node)parser.value(result);
+  }
+
+  public void process(Node node) {
+    if (runtime.test("printJavaAST")) {
+      runtime.console().format(node).pln().flush();
+    }
+
+    if (runtime.test("printJavaCode")) {
+      new JavaPrinter(runtime.console()).dispatch(node);
+      runtime.console().flush();
+    }
+
+    if (runtime.test("countMethods")) {
+      new Visitor() {
+        private int count = 0;
+
+        public void visitCompilationUnit(GNode n) {
+          visit(n);
+          runtime.console().p("Number of methods: ").p(count).pln().flush();
+        }
+
+        public void visitMethodDeclaration(GNode n) {
+          runtime.console().p("Name of node: ").p(n.getName()).pln();
+          runtime.console().p("Name of method: ").p(n.getString(3)).pln();
+          visit(n);
+          count++;
+        }
+
+        public void visit(Node n) {
+          for (Object o : n) if (o instanceof Node) dispatch((Node) o);
+        }
+
+      }.dispatch(node);
+    }
+  }
+
+  /**
+   * Run the translator with the specified command line arguments.
+   *
+   * @param args The command line arguments.
+   */
+  public static void main(String[] args) {
+    //Translator t = new Translator();
+    //t.run(args);
+
+    new Translator().run(args);
+  }
+
+}
