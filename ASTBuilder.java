93bcddc0d285000790d6cc098f9902448aaa1989
vagrant <mpgarate@gmail.com>
2013-10-06 22:14:21 +0000

    rewrote ASTBuilder to support visitor methods in TestTranslator.java

--- ASTBuilder.java
+++ ASTBuilder.java
@@ -2,45 +2,95 @@ package xtc.oop;
 
 import xtc.tree.GNode;
 
+/* All the imports from Translator.java */
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
+import xtc.lang.JavaFiveParser;
+import xtc.lang.JavaPrinter;
+
+import xtc.parser.ParseException;
+import xtc.parser.Result;
+
+import xtc.tree.GNode;
+import xtc.tree.Node;
+import xtc.tree.Visitor;
+
+import xtc.util.Tool;
+
+/* End Translator.java imports */
+
 public class ASTBuilder{
-	public GNode root, packageDeclaration, includeDeclaration, classDeclaration;
+	public GNode root, packageDeclaration, includeDeclaration, classDeclaration, classBody;
 
-	public ASTBuilder(){
-		root = 								GNode.create("TranslationUnit");
-		packageDeclaration = 	GNode.create("PackageDeclaration");
-		includeDeclaration = 	GNode.create("IncludeDeclaration");
-		classDeclaration = 		classNodes();
+	public ASTBuilder(GNode n){
+		root = GNode.create("TranslationUnit");
+	}
 
-		root.addNode(packageDeclaration);
-		root.addNode(includeDeclaration);
+	public void createClassDeclaration(GNode n){
+		classDeclaration = GNode.create("ClassDeclaration");
+		/* 0 get the name of the class */
+		/* 1 modifiers */
+		/* 2 extension */
+		/* 3 classBody */
+		classBody = GNode.create("ClassBody");
+		classDeclaration.add(3, classBody);
+
 		root.addNode(classDeclaration);
 	}
 
-	public GNode classNodes(){
-		GNode classDec = GNode.create("ClassDeclaration");
-		GNode classBody = GNode.create("ClassBody");
-		GNode classModifier = GNode.create("ClassModifier");
-		GNode classExtension = GNode.create("ClassExtension");
+	public void createMethodDeclaration(GNode n){
+		GNode methodDeclaration = GNode.create("MethodDeclaration");
 
-		classDec.addNode(classBody);
-		classDec.addNode(classModifier);
-		classDec.addNode(classExtension);
+		/* 0 Return type */
+		/* 1 Name */
+		/* 2 Arguments */
+		/* 3 Block */
 
-		GNode constructorDeclaration = GNode.create("ConstructorDeclaration");
-		GNode functionDeclaration = GNode.create("FunctionDeclaration");
-		GNode fieldDeclaration = GNode.create("FieldDeclaration");
-		GNode block = GNode.create("Block");
-
-		classBody.addNode(constructorDeclaration);
-		classBody.addNode(functionDeclaration);
-		classBody.addNode(fieldDeclaration);
-		classBody.addNode(block);
-
-		return classDec;
+		addBlock(n.get(7), methodDeclaration);
+		classBody.addNode(methodDeclaration);
 	}
 
-	public static void main(String args[]){
-		ASTBuilder cpp = new ASTBuilder();
-		System.out.println(cpp.root);
+	private void addBlock(Object o, GNode parent){
+		GNode n = (GNode)o;
+
+		/* 0 unordered */
+		GNode block = GNode.create("Block");
+		parent.add(3, block);
+		addFieldDeclaration(n.get(0), block);
+
+	}
+
+	private void addFieldDeclaration(Object o, GNode parent){
+		GNode n = (GNode)o;
+
+		GNode fieldDeclaration = GNode.create("Field Declaration");
+		/* 0 Modifiers */
+		String modifier = null;
+		fieldDeclaration.add(0, null);
+		/* 1 Type */
+		String type = n.getGeneric(1).getGeneric(0).getString(0);
+		fieldDeclaration.add(1, type);
+
+		/* 2 Declarators */
+		addDeclarator(n.get(2), fieldDeclaration);
+
+		parent.addNode(fieldDeclaration);
+	}
+
+	private void addDeclarator(Object o, GNode parent){
+		GNode n = (GNode)o;
+		GNode declarator = GNode.create("Declarator");
+
+		String left_side = n.getGeneric(2).getGeneric(0).getString(0);
+		declarator.add(0,left_side);
+
+		String right_side = n.getGeneric(2).getString(0);
+		declarator.add(1,right_side);
+
+		parent.add(2, declarator);
 	}
 }
