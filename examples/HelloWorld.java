9d53bc8fe0a25e5434a0940adab05705c5b18c07
ahmadchatha <chathaahmad@yahoo.com>
2013-10-12 23:37:58 -0400

    main test file for inheritance

--- examples/HelloWorld.java
+++ examples/HelloWorld.java
@@ -1,6 +1,17 @@
-package xtc.oop.examples;
-import java.util.scanner;
-public class HelloWorld extends Object{
+package examples;
+import oop.SimpleNumber;
+public class HelloWorld {
+
+	public int world;
+
+	public HelloWorld(){
+		world = 0;
+	}
+
+	public void createWorld(){
+		world = 1;
+	}
+
 	public int main(String a){
 		int x = 5;
 		return x;
