aa87ddc221a033b2dd7bbb20470f7fa17b4c6a67
vagrant <mpgarate@gmail.com>
2013-10-20 23:51:45 +0000

    fixed some bugs in translateme.cc

--- cppExamples/TranslateMe.cc
+++ cppExamples/TranslateMe.cc
@@ -3,15 +3,16 @@
 #include "output.h"
 
 using namespace java::lang;
+using namespace subExamples;
 using namespace std;
 
 int main(void){
 
-	const HelloWorld hw = new HelloWorld();
-	cout << hw->__vptr->toString()->data <<endl;
+	const HelloWorld hw = new __HelloWorld();
+	cout << hw->__vptr->toString(hw)->data <<endl;
 
-	const HelloUniverse hu = new HelloUniverse();
-	cout << hu->__vptr->toString()->data <<endl;
+	const HelloUniverse hu = new __HelloUniverse();
+	cout << hu->__vptr->toString(hu)->data <<endl;
 	cout << hu->__vptr->createWorld()->data <<endl;
 
 	return 0;
