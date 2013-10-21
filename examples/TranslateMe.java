30616c70bdd71a9719f3816a631c419ff33ded61
ahmadchatha <chathaahmad@yahoo.com>
2013-10-21 18:22:08 -0400

    --

--- examples/TranslateMe.java
+++ examples/TranslateMe.java
@@ -1,5 +1,5 @@
-import subExamples.HelloWorld;
-import subExamples.HelloUniverse;
+import subExamples.*;
+
 public class TranslateMe {
 	public static void main(String[] args){
 		HelloWorld hw = new HelloWorld();
