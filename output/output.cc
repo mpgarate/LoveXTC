1992dbc2f6552d38ad3d8e5c7024a9195d7a0462
vagrant <mpgarate@gmail.com>
2013-10-21 22:50:31 +0000

    added output directory for reference

---
+++ output/output.cc
@@ -0,0 +1,38 @@
+#include "output.h"
+#include <sstream>
+/* Visiting translation unit for examples/TranslateMe.java:1:1 */
+/* visiting Import declaration */
+/* visiting class declaration */
+/* visiting class body */
+/* visiting method declaration */
+
+/* visiting block */
+/* visiting Field Declaration */
+/* visiting Field Declaration */
+/* Visiting translation unit for examples/subExamples/HelloWorld.java:1:1 */
+/* visiting package declaration */
+/* visiting class declaration */
+/* visiting class body */
+/* visiting Field Declaration */
+
+/* visiting block */
+/* visiting method declaration */
+
+/* visiting block */
+/* visiting method declaration */
+
+/* visiting block */
+/* visiting Field Declaration */
+/* Visiting translation unit for examples/subExamples/HelloUniverse.java:1:1 */
+/* visiting package declaration */
+/* visiting class declaration */
+/* visiting class body */
+
+/* visiting block */
+/* visiting method declaration */
+
+/* visiting block */
+/* visiting Field Declaration */
+/* visiting method declaration */
+
+/* visiting block */
