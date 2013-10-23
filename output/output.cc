945514cd6a77c4ed9565d70b292e81c2973f6f41
vagrant <mpgarate@gmail.com>
2013-10-23 22:37:04 +0000

    updating things

--- output/output.cc
+++ output/output.cc
@@ -1,24 +1,17 @@
 #include "output.h"
 #include <sstream>
-subExamples __TranslateMe::main(String args) {
-HelloWorld hw = new __HelloWorld();
-cout << hw->__vptr->toString(hw)->data <<endl;
-HelloUniverse hu = new __HelloUniverse();
-cout << hu->__vptr->toString(hu)->data <<endl;
-Object o = (Object)hu;
-cout << o->__vptr->toString(o)->data <<endl;
-cout << hu->__vptr->createWorld(hu) <<endl;
-}
 namespace subExamples {
 __HelloWorld::__HelloWorld() : __vptr(&__vtable), world(0) {}
 
 int32_t __HelloWorld::createWorld(HelloWorld __this) {
 __this->world = 1;
 return __this->world;
+
 }
 int32_t __HelloWorld::returnX(String a) {
 int32_t x = 5;
 return x;
+
 }
 __HelloWorld_VT __HelloWorld::__vtable;
 
@@ -34,9 +27,11 @@ __HelloUniverse::__HelloUniverse() : __vptr(&__vtable), {}
 int32_t __HelloUniverse::returnX(String b) {
 int32_t x = 10;
 return x;
+
 }
 String __HelloUniverse::toString(HelloUniverse __this) {
 return new __String("x");
+
 }
 __HelloUniverse_VT __HelloUniverse::__vtable;
 
