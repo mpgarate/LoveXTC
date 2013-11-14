0830504a7e87aee2ebc8b8d56c3f43049ee83433
Carl <ccr263@nyu.edu>
2013-11-14 17:53:06 -0500

    Made some fixes to Overloading.  There is a bug still with the last method declaration in the vTable/dataLayout

--- NodeHandler.java
+++ NodeHandler.java
@@ -33,7 +33,16 @@ public class NodeHandler {
     // InheritanceTree
 	protected GNode handleClassBody(GNode inheritNode, GNode astNode, boolean isVTable) {
 		boolean foundConstructor = false;
+			GNode nodesToOverload = GNode.create("NodesToOverload");
 
+		if (inheritNode.size() > 0) {
+			for (int k=0;k<inheritNode.size();k++) {
+				if (inheritNode.getNode(k).size() == 5 && inheritNode.getNode(k).getNode(4).size() != 0 && inheritNode.getNode(k).getString(3)=="Object") {
+			//Renames the parameters in a method to be the classname type
+					inheritNode.getNode(k).getNode(4).set(0,inheritNode.getProperty("parent"));
+				}
+			}
+		}		
 
 		for (int i = 0; i < astNode.size(); i++) {
 			if (astNode.get(i) != null && astNode.get(i) instanceof Node) {
@@ -43,14 +52,11 @@ public class NodeHandler {
 				} else if (child.hasName("MethodDeclaration")) {
 						handleMethodDeclaration(inheritNode, (GNode) child,
 							isVTable);
-						checkForOverloading(inheritNode, (GNode)inheritNode.getNode(inheritNode.size()-1));
 			    // METHOD OVERWRITING
 						boolean isOverwritten = false;
 						for (int j = 0; j < inheritNode.size() - 1; j++) {
 							if (inheritNode.getNode(j).size() == 5) {
-								String searchName = (String) inheritNode.getNode(j).get(2);
-								String checkName = (String) inheritNode.getNode(inheritNode.size() - 1).get(2);
-								if (searchName.equals(checkName)) {
+								if (nodeEquals((GNode)inheritNode.getNode(j), (GNode)inheritNode.getNode(inheritNode.size()-1), true)) {
 									inheritNode.set(j, inheritNode.getNode(inheritNode.size()-1));
 									isOverwritten = true;
 									break;
@@ -60,6 +66,7 @@ public class NodeHandler {
 						if(isOverwritten){
 							inheritNode.remove(inheritNode.size() -1);
 						}
+						checkForOverloading(inheritNode, (GNode)inheritNode.getNode(inheritNode.size()-1), nodesToOverload);
 					}
 					else if (child.hasName("ConstructorDeclaration") && !isVTable) {
 					foundConstructor = true;
@@ -73,33 +80,90 @@ public class NodeHandler {
 			String className = inheritNode.getProperty("parent").toString();
 			inheritNode.add(2,createConstructor(className, null));	
 		}
+		executeOverloading(nodesToOverload);
 		return inheritNode;
 	}
 
-	protected void checkForOverloading(GNode masterNode, GNode currentNode) {
+	//Traverses two subtrees rooted by node1 and node2 and returns if they are equal or not.
+	//Set methodOverwriting to true the first time you call this recursive function if you are checking to see if node2 overwrites node1.
+	private boolean nodeEquals(GNode node1, GNode node2, boolean methodOverwriting) {
+		if (!node1.getName().equals(node2.getName())) {
+			return false;
+		}
+		else if (node1.size() != node2.size()) {
+			return false;
+		}
+		else {
+			boolean temp = true;
+			for (int i=0;i<node1.size();i++) {
+				if (methodOverwriting && i==3) {
+					return true;
+				}
+
+				if (node1.get(i) instanceof String && node2.get(i) instanceof String) {
+					temp = node1.getString(i).equals(node2.getString(i));
+				}
+				else if (node1.get(i) instanceof Node && node2.get(i) instanceof Node) {
+					if (!node1.getName().equals(node2.getName())) {
+						return false;
+					}
+					temp = nodeEquals((GNode)node1.getNode(i), (GNode)node2.getNode(i), false);
+				}
+				else {
+					return false;
+				}
+
+				if (temp == false) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	//Runs through each node that is a child of masterNode and compares it to currentNode.  If they have the same name, then switch the names of the two to be the appropriate overloaded names.
+	protected void checkForOverloading(GNode masterNode, GNode currentNode, GNode nodesToOverload) {
 		if (masterNode.size() > 0) {
-			for (int i=0;i<masterNode.size();i++) {
+			for (int i=0;i<masterNode.size()-1;i++) {
 				if (masterNode.getNode(i).size() == 5) {
 					String masterString = masterNode.getNode(i).getString(2);
 					String currentString = currentNode.getString(2);
 					if (masterString.equals(currentString)) {
-						String newCurrentString = "";
-						if (currentNode.getNode(4).size() > 0) {
-							for (int j=0;j<currentNode.getNode(4).size();j++) {
-								newCurrentString = newCurrentString+"_"+currentNode.getNode(4).getString(j);
+						boolean addCurrentNode = true;
+						boolean addMasterNode = true;
+						if (nodesToOverload.size() > 0) {
+							for (int j=0;j<nodesToOverload.size();j++) {
+								if (nodeEquals((GNode)masterNode.getNode(i), (GNode)nodesToOverload.getNode(j), false)) {
+									addMasterNode = false;
+									break;
+								}
 							}
 						}
-						String newMasterString = "";
-						if (masterNode.getNode(i).getNode(4).size() > 0) {
-							for (int k=0;k<masterNode.getNode(i).getNode(4).size();k++) {
-								newMasterString = newMasterString+"_"+masterNode.getNode(i).getNode(4).getString(k);
-							}
+						
+						nodesToOverload.add(currentNode);
+						if (addMasterNode) {
+							//nodesToOverload.add((GNode)masterNode.getNode(i));
 						}
-						currentNode.set(2, currentString);
-						masterNode.getNode(i).set(2, masterString);
 						break;
 					}
 				}
+			}
+		}
+	}
+
+	protected void executeOverloading(GNode nodesToOverload) {
+		if (nodesToOverload.size()==0) {
+			return;
+		}
+
+		for (int i=0;i<nodesToOverload.size();i++) {
+			String newNodeString = nodesToOverload.getNode(i).getString(2);
+			if (nodesToOverload.getNode(i).getNode(4).size() > 0) {
+				for (int j=0;j<nodesToOverload.getNode(i).getNode(4).size();j++) {
+					newNodeString = newNodeString+"_"+nodesToOverload.getNode(i).getNode(4).getString(j);
+				}
+				nodesToOverload.getNode(i).set(2, newNodeString);
 			}
 		}
 	}
@@ -152,6 +216,12 @@ public class NodeHandler {
 					Node param = child;
 					if (param.size() > 0)
 						parameters = new String[param.size()];
+					else if (param.size()==0) {
+						//Adds a parameter of the class to the method declaration when there's
+						//no parameters specified.
+						parameters = new String[1];
+						parameters[0] = classname;
+					}
 					for (int j = 0; j < param.size(); j++) {
 						if (param.get(j) != null
 							&& param.get(j) instanceof Node) {
