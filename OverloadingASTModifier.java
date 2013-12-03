72d2238fa1c8c7ee67078e8b61dc189ab47966a4
Carl <ccr263@nyu.edu>
2013-12-03 14:55:27 -0500

    Fixed extraneous print statements

--- OverloadingASTModifier.java
+++ OverloadingASTModifier.java
@@ -76,7 +76,6 @@ public class OverloadingASTModifier extends Visitor {
 	public void visitMethodDeclaration(GNode n) {
 		if (n.size()>3 && n.get(3) instanceof String) {
 			methodList.add(n); //creates a list with all the method nodes.
-			System.out.println("Size of method list increased to: "+methodList.size());
 		}
 
 		//visit(n);
@@ -115,7 +114,6 @@ public class OverloadingASTModifier extends Visitor {
        		}
        		parameterList = new LinkedList<String>();
       	}
-      	System.out.println(overload.toString());
       	overload.set(3, newNodeString);
     }
 
