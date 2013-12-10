2df2c10d1a7a49cc9911663eb9f1562bd134f1e7
vagrant <mpgarate@gmail.com>
2013-12-10 11:00:59 -0500

    switched to testing from new test-cases directory

--- LoveTest/TestDriver.java
+++ LoveTest/TestDriver.java
@@ -37,69 +37,69 @@ public class TestDriver{
 /*
   @Test public void translateWiesMidtermExamples(){
     for (int i = 1; i < 3; i++){ //This should go to 22
-      String path = "examples/wies-tests/Test" + i + ".java";
+      String path = "examples/test-cases/Test0" + i + ".java";
       String name = "Wies Test" + i + ".java";
       t.translateAndCompare(path, name);
     } 
     for (int i = 100; i < 106; i++){
-      String path = "examples/wies-tests/Test" + i + ".java";
+      String path = "examples/test-cases/Test0" + i + ".java";
       String name = "Wies Test" + i + ".java";
       t.translateAndCompare(path, name);
     }
   }
 */
   @Test public void translateWies1(){
-    String path = "examples/wies-tests/Test" + 1 + ".java";
+    String path = "examples/test-cases/Test00" + 1 + ".java";
     String name = "Wies Test" + 1 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies2(){
-    String path = "examples/wies-tests/Test" + 2 + ".java";
+    String path = "examples/test-cases/Test00" + 2 + ".java";
     String name = "Wies Test" + 2 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies3(){
-    String path = "examples/wies-tests/Test" + 3 + ".java";
+    String path = "examples/test-cases/Test00" + 3 + ".java";
     String name = "Wies Test" + 3 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies4(){
-    String path = "examples/wies-tests/Test" + 4 + ".java";
+    String path = "examples/test-cases/Test00" + 4 + ".java";
     String name = "Wies Test" + 4 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies5(){
-    String path = "examples/wies-tests/Test" + 5 + ".java";
+    String path = "examples/test-cases/Test00" + 5 + ".java";
     String name = "Wies Test" + 5 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies6(){
-    String path = "examples/wies-tests/Test" + 6 + ".java";
+    String path = "examples/test-cases/Test00" + 6 + ".java";
     String name = "Wies Test" + 6 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies9(){
-    String path = "examples/wies-tests/Test" + 9 + ".java";
+    String path = "examples/test-cases/Test00" + 9 + ".java";
     String name = "Wies Test" + 9 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies10(){
-    String path = "examples/wies-tests/Test" + 10 + ".java";
+    String path = "examples/test-cases/Test0" + 10 + ".java";
     String name = "Wies Test" + 10 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies11(){
-    String path = "examples/wies-tests/Test" + 11 + ".java";
+    String path = "examples/test-cases/Test0" + 11 + ".java";
     String name = "Wies Test" + 11 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies12(){
-    String path = "examples/wies-tests/Test" + 12 + ".java";
+    String path = "examples/test-cases/Test0" + 12 + ".java";
     String name = "Wies Test" + 12 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies13(){
-    String path = "examples/wies-tests/Test" + 13 + ".java";
+    String path = "examples/test-cases/Test0" + 13 + ".java";
     String name = "Wies Test" + 13 + ".java";
     t.translateAndCompare(path, name);
   }
@@ -110,27 +110,27 @@ public class TestDriver{
     compiles, runs, and prints nothing to stdout, just like the java file. 
   */
   @Test public void translateWies14(){
-    String path = "examples/wies-tests/Test" + 14 + ".java";
+    String path = "examples/test-cases/Test0" + 14 + ".java";
     String name = "Wies Test" + 14 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies15(){
-    String path = "examples/wies-tests/Test" + 19 + ".java";
+    String path = "examples/test-cases/Test0" + 19 + ".java";
     String name = "Wies Test" + 19 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies100(){
-    String path = "examples/wies-tests/Test" + 100 + ".java";
+    String path = "examples/test-cases/Test" + 100 + ".java";
     String name = "Wies Test" + 100 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies101(){
-    String path = "examples/wies-tests/Test" + 101 + ".java";
+    String path = "examples/test-cases/Test" + 101 + ".java";
     String name = "Wies Test" + 101 + ".java";
     t.translateAndCompare(path, name);
   }
   @Test public void translateWies102(){
-    String path = "examples/wies-tests/Test" + 102 + ".java";
+    String path = "examples/test-cases/Test" + 102 + ".java";
     String name = "Wies Test" + 102 + ".java";
     t.translateAndCompare(path, name);
   }
--- examples/wies-tests/Test21.java
+++
@@ -1,9 +0,0 @@
-public class Test21 {
-  static int x = 4;
-
-  public static void main(String[] args) {
-    int x;
-    x = 3;
-    System.out.println(Test21.x);
-  }
-}
