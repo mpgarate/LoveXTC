63f17e329ea3152b92175a5d18645b46c0626290
vagrant <mpgarate@gmail.com>
2013-12-10 10:51:55 -0500

    added test 19

--- LoveTest/TestDriver.java
+++ LoveTest/TestDriver.java
@@ -114,6 +114,11 @@ public class TestDriver{
     String name = "Wies Test" + 14 + ".java";
     t.translateAndCompare(path, name);
   }
+  @Test public void translateWies15(){
+    String path = "examples/wies-tests/Test" + 19 + ".java";
+    String name = "Wies Test" + 19 + ".java";
+    t.translateAndCompare(path, name);
+  }
   @Test public void translateWies100(){
     String path = "examples/wies-tests/Test" + 100 + ".java";
     String name = "Wies Test" + 100 + ".java";
