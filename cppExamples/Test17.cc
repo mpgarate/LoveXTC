74d4d248364568150eba64a18281e70b54faa0f7
vagrant <mpgarate@gmail.com>
2013-12-13 18:16:21 -0500

    wrote working cpp example for test 17

--- cppExamples/Test17.cc
+++ cppExamples/Test17.cc
@@ -1,30 +1,46 @@
+#include "output.h"
+#include <sstream>
 #include <iostream>
-#include "A.h"
-#include "Test17.h"
+#include "java_lang.h"
+#include "output.h"
 
 using namespace java::lang;
+using namespace std;
 
-__Test17::__Test17() : __vptr(&__vtable) { }
+__A::__A() : __vptr(&__vtable){}
 
-Test17 __Test17::init(Test17 __this) {
-  // make implicit call to constructor of Test17's superclass explicit
-  __Object::init(__this);
-  return __this;
+A __A::init(A __this, int32_t x){
+__Object::init(__this);
+__this->self = __this;
+return __this;
 }
 
-__Test17_VT __Test17::__vtable;
+//A __A::self(A __this) {
+//return __this->self;
 
+//}
+__A_VT __A::__vtable;
 
-Class __Test17::__class() {
-  static Class k =
-    new __Class(__rt::literal("Test17"), java::lang::__Object::__class());
-  return k;
+Class __A::__class() {
+static Class k = new __Class(__rt::literal("A"), (Class) __rt::null());
+return k;
+}
+A __A::self_impl(A __this) {
+  return __this->self;
 }
 
-void __Test17::main(__rt::Ptr<__rt::Array<String> > args) {
-  A a = __A::init(new __A());
-  __rt::checkNotNull(a);
-  A tmp = a->__vptr->self(a);
-  __rt::checkNotNull(tmp);
-  std::cout << tmp->__vptr->toString(tmp) << std::endl;
+int main(void){
+A a = __A::init(new __A(), 5);
+__rt::checkNotNull(a);
+A tmp = a->__vptr->self(a);
+__rt::checkNotNull(tmp);
+std::cout << tmp->__vptr->toString(tmp) << std::endl;
+
+return 0;
+}
+__Test017_VT __Test017::__vtable;
+
+Class __Test017::__class() {
+static Class k = new __Class(__rt::literal("Test017"), (Class) __rt::null());
+return k;
 }
--- cppExamples/Test17.h
+++ cppExamples/Test17.h
@@ -1,46 +1,76 @@
 #pragma once
-
+#include <stdint.h>
+#include <string>
 #include "java_lang.h"
 
 using namespace java::lang;
-
-struct __Test17;
-struct __Test17_VT;
-typedef __rt::Ptr<__Test17> Test17;
-
-struct __Test17 {
-  __Test17_VT* __vptr;
-  
-  // The constructor.
-  __Test17();
-
-  // The methods implemented by Test17.
-  static void main(__rt::Ptr<__rt::Array<String> > args);
-  static Test17 init(Test17);
-
-  // The function returning the class object representing
-  // Test17.
-  static Class __class();
-
-  // The vtable for Test17.
-  static __Test17_VT __vtable;
+using namespace std;
+typedef unsigned char byte;
+struct __A;
+struct __A_VT;
+typedef __rt::Ptr<__A> A;
+struct __A {
+__A_VT* __vptr;
+A self;
+__A();
+static A init(A, int32_t);
+static A self_impl(A);
+//static int32_t hashCode(A);
+//static bool equals(A,Object);
+//static Class getClass(A);
+//static String toString(A);
+static Class __class();
+//static A self(A);
+static __A_VT __vtable;
 };
 
-// The vtable layout for Test17.
-struct __Test17_VT {
-  Class __isa;
-  void (*__delete)(__Test17*);
-  int32_t (*hashCode)(Test17);
-  bool (*equals)(Test17, Object);
-  Class (*getClass)(Test17);
-  String (*toString)(Test17);
-
-  __Test17_VT()
-  : __isa(__Test17::__class()),
-    __delete(&__rt::__delete<__Test17>),
-    hashCode((int32_t(*)(Test17))&__Object::hashCode),
-    equals((bool(*)(Test17, Object))&__Object::equals),
-    getClass((Class(*)(Test17))&__Object::getClass),
-    toString((String(*)(Test17))&__Object::toString) {
-  }
+struct __A_VT {
+Class __isa;
+void (*__delete)(__A*);
+int32_t (*hashCode)(A);
+bool (*equals)(A,Object);
+Class (*getClass)(A);
+String (*toString)(A);
+A (*self)(A);
+__A_VT()
+: __isa(__A::__class()),
+__delete(&__rt::__delete<__A>),
+hashCode((int32_t(*)(A)) &__Object::hashCode),
+equals((bool(*)(A,Object)) &__Object::equals),
+getClass((Class(*)(A)) &__Object::getClass),
+toString((String(*)(A)) &__Object::toString),
+self(__A::self_impl)
+{}
 };
+
+struct __Test017;
+struct __Test017_VT;
+typedef __rt::Ptr<__Test017> Test017;
+struct __Test017 {
+__Test017_VT* __vptr;
+static __Test017_VT __vtable;
+__Test017();
+static Test017 init(Test017);
+static int32_t hashCode(Test017);
+static bool equals(Test017,Object);
+static Class getClass(Test017);
+static String toString(Test017);
+static Class __class();
+};
+
+struct __Test017_VT {
+Class __isa;
+void (*__delete)(__Test017*);
+int32_t (*hashCode)(Test017);
+bool (*equals)(Test017,Object);
+Class (*getClass)(Test017);
+String (*toString)(Test017);
+__Test017_VT()
+: __isa(__Test017::__class()),
+__delete(&__rt::__delete<__Test017>),
+hashCode((int32_t(*)(Test017)) &__Object::hashCode),
+equals((bool(*)(Test017,Object)) &__Object::equals),
+getClass((Class(*)(Test017)) &__Object::getClass),
+toString((String(*)(Test017)) &__Object::toString){}
+};
+
