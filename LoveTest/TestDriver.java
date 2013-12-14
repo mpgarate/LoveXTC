eb187d3cbdee6b33d9ce6ce9fe6f46acde9f8d1a
vagrant <mpgarate@gmail.com>
2013-12-13 19:56:55 -0500

    updated test driver

--- LoveTest/TestDriver.java
+++ LoveTest/TestDriver.java
@@ -137,6 +137,11 @@ public class TestDriver{
     String name = "Wies Test" + 32 + ".java";
     t.translateAndCompare(path, name);
   }
+  @Test public void translateWies34(){
+    String path = "examples/test-cases/Test0" + 34 + ".java";
+    String name = "Wies Test" + 34 + ".java";
+    t.translateAndCompare(path, name);
+  }
 
   @Test public void translateWies36(){
     String path = "examples/test-cases/Test0" + 36 + ".java";
